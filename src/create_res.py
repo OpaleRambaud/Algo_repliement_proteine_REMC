96e828f4ad8708e2724d5fce75f408c646dd6253
Julien Pichon <julien.pichon@cri-paris.org>
2020-09-14 18:33:53 +0200

    dirty corner move

--- src/create_res.py
+++ src/create_res.py
@@ -23,10 +23,10 @@ class Residue:
 	
 	def end_move(self, neighbour, protein_coordinates):
 		available_mvt = []
-		right = neighbour.coordinates[0] + 1, neighbour.coordinates[1]
-		left = neighbour.coordinates[0] - 1, neighbour.coordinates[1]
-		bottom = neighbour.coordinates[0], neighbour.coordinates[1] + 1
-		top = neighbour.coordinates[0], neighbour.coordinates[1] - 1		
+		right = [neighbour.coordinates[0] + 1, neighbour.coordinates[1]]
+		left = [neighbour.coordinates[0] - 1, neighbour.coordinates[1]]
+		bottom = [neighbour.coordinates[0], neighbour.coordinates[1] - 1]
+		top = [neighbour.coordinates[0], neighbour.coordinates[1] + 1]	
 		if tuple(right) not in protein_coordinates:
 			available_mvt.append(right)
 		if tuple(left) not in protein_coordinates:
@@ -39,11 +39,76 @@ class Residue:
 		protein_coordinates.remove(tuple(self.coordinates))
 		self.coordinates = mvt
 		protein_coordinates.add(tuple(self.coordinates))
+	
+	def corner_move(self, neighbour1, neighbour2, protein_coordinates):
+		# Right bottom movement. 
+		if neighbour1.coordinates[0] > self.coordinates[0] and neighbour2.coordinates[1] < self.coordinates[1]:
+			if tuple([neighbour1.coordinates[0], neighbour2.coordinates[1]]) not in protein_coordinates:
+				protein_coordinates.remove(tuple(self.coordinates))
+				self.coordinates = [neighbour1.coordinates[0], neighbour2.coordinates[1]]
+				protein_coordinates.add(tuple(self.coordinates))
+			else:
+				print("redo step")
+		elif neighbour2.coordinates[0] > self.coordinates[0] and neighbour1.coordinates[1] < self.coordinates[1]:
+			if tuple([neighbour2.coordinates[0], neighbour1.coordinates[1]]) not in protein_coordinates:
+				protein_coordinates.remove(tuple(self.coordinates))
+				self.coordinates = [neighbour2.coordinates[0], neighbour1.coordinates[1]]
+				protein_coordinates.add(tuple(self.coordinates))
+			else:
+				print("redo step")
+		# Left top movement.
+		elif neighbour1.coordinates[0] < self.coordinates[0] and neighbour2.coordinates[1] > self.coordinates[1]:
+			if tuple([neighbour1.coordinates[0], neighbour2.coordinates[1]]) not in protein_coordinates:
+				protein_coordinates.remove(tuple(self.coordinates))
+				self.coordinates = [neighbour1.coordinates[0], neighbour2.coordinates[1]]
+				protein_coordinates.add(tuple(self.coordinates))
+			else:
+				print("redo step")
+		elif neighbour2.coordinates[0] < self.coordinates[0] and neighbour1.coordinates[1] > self.coordinates[1]:
+			if tuple([neighbour2.coordinates[0], neighbour1.coordinates[1]]) not in protein_coordinates:
+				protein_coordinates.remove(tuple(self.coordinates))
+				self.coordinates = [neighbour2.coordinates[0], neighbour1.coordinates[1]]
+				protein_coordinates.add(tuple(self.coordinates))
+			else:
+				print("redo step")
+		# Right top movement.
+		elif neighbour1.coordinates[0] > self.coordinates[0] and neighbour2.coordinates[1] > self.coordinates[1]:
+			if tuple([neighbour1.coordinates[0], neighbour2.coordinates[1]]) not in protein_coordinates:
+				protein_coordinates.remove(tuple(self.coordinates))
+				self.coordinates = [neighbour2.coordinates[0], neighbour1.coordinates[1]]
+				protein_coordinates.add(tuple(self.coordinates))
+			else:
+				print("redo step")
+		elif neighbour2.coordinates[0] > self.coordinates[0] and neighbour1.coordinates[1] > self.coordinates[1]:
+			if tuple([neighbour2.coordinates[0], neighbour1.coordinates[1]]) not in protein_coordinates:
+				protein_coordinates.remove(tuple(self.coordinates))
+				self.coordinates = [neighbour2.coordinates[0], neighbour1.coordinates[1]]
+				protein_coordinates.add(tuple(self.coordinates))
+			else:
+				print("redo step")
+		# Left bottom movement
+		elif neighbour1.coordinates[0] < self.coordinates[0] and neighbour2.coordinates[1] < self.coordinates[1]:
+			if tuple([neighbour1.coordinates[0], neighbour2.coordinates[1]]) not in protein_coordinates:
+				protein_coordinates.remove(tuple(self.coordinates))
+				self.coordinates = [neighbour1.coordinates[0], neighbour2.coordinates[1]]
+				protein_coordinates.add(tuple(self.coordinates))
+			else:
+				print("redo step")
+		elif neighbour2.coordinates[0] < self.coordinates[0] and neighbour1.coordinates[1] < self.coordinates[1]:
+			if tuple([neighbour2.coordinates[0], neighbour1.coordinates[1]]) not in protein_coordinates:
+				protein_coordinates.remove(tuple(self.coordinates))
+				self.coordinates = [neighbour2.coordinates[0], neighbour1.coordinates[1]]
+				protein_coordinates.add(tuple(self.coordinates))
+			else:
+				print("redo step")
+		else:
+			print("redo step")
 			
+		
 			
 if __name__ == "__main__":
 	
-	seq, coordinates = get_seq("../data/HP1.txt")
+	seq, coordinates = get_seq("../data/HP2.txt")
 	
 	# creating list        
 	protein = []
@@ -56,3 +121,30 @@ if __name__ == "__main__":
 	protein_coordinates = set()
 	for res in protein:
 		protein_coordinates.add(tuple(res.coordinates))
+
+	"""
+	print(protein[2].coordinates)
+	print("\n")
+	print(protein_coordinates)
+	
+	protein[-1].end_move(protein[-2], protein_coordinates)
+	print("\n")
+	print(protein[-1].coordinates)
+	print("\n")
+	print(protein_coordinates)
+	
+	
+	protein[2].corner_move(protein[1], protein[3], protein_coordinates)
+	print("\n")
+	print(protein[2].coordinates)
+	print("\n")
+	print(protein_coordinates)
+	"""
+	
+	
+	
+	
+	
+	
+	
+	
